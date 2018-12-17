2d372cb3771aa093395c0796fbce17eb9ce7afff
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2018-12-17 23:07:08 +0700

    Collections TUPLE

---
+++ Demo/Collections TUPLE.py
@@ -0,0 +1,18 @@
+my_tuple = ("Apples", "Oranges", "Grapes")
+
+print(my_tuple)
+print(my_tuple[1])
+print(my_tuple[-1])
+print(my_tuple[0:2])
+
+for val in my_tuple:
+    print(val)
+print(len(my_tuple))
+my_tuple_2 = ("Banana", (1,2,3), ["Tokyo", "New Delhi"])
+print(my_tuple_2[2][1])
+
+my_tuple_2[2][1] = "New York"
+print(my_tuple_2)
+
+print("Banana" in my_tuple_2) # True
+print("Cherry" in my_tuple_2) # False
