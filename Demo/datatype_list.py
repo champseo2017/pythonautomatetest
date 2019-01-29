b33cc0e00112d80ee98fc7abd5d688100997cd2d
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-01-30 01:47:41 +0700

    Create datatype_list.py

---
+++ Demo/datatype_list.py
@@ -0,0 +1,8 @@
+# ชนิดข้อมูล list ข้อมูล list ใน python คล้ายกับ array ในภาษาอื่น [..] | string->[""] ชนิดข้อมูล list สามารถทำการ เพิ่ม ลบ แก้ไข ข้อมูลได้
+# ข้อมูลชนิดทูเพิลไม่สามารถทำการเปลียนแปลงได้
+books_lst_lst = ["Python", "Java", "C", "C++", "c#"]
+numbers_lst = [1, 2, 3, 4, 5, 6]
+book_num_lst = [1, "Python", 2, "Java", 3, "C", 4, "C++", 5, "c#"]
+print("รายชื่อหนังสือใน books_lst = ", books_lst_lst)
+print("ตัวเลขใน numbers_lst = ", numbers_lst)
+print("สมาชิกใน book_num_lst = ", book_num_lst)
