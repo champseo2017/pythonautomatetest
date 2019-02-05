dcc0d3fc1f7c48e6d0379d76f3bc7eb41a08b15d
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-02-05 08:08:57 +0700

    update

---
+++ Demo/mark_string_01.py
@@ -0,0 +1,7 @@
+# การใช้สัญลักษณ์ร่วมกับข้อความหลายบรรทัด ( เมื่อใช้สัญลักษณ์เพื่อกำหนดจุดเริ่มต้นและจุดสิ้นสุดของข้อความ ให้พิมพ์ Double Quote อย่างล่ะ 3 เครื่องหมาย )
+a = """
+this is my   bookfwefewfewfwe
+fewfewfewf ewfew fwefewfwefew
+
+"""
+print(a)
